a4d22198a06f8ea42bef0f3ded6543de18e319fd
jasonweicn <jasonwei06@hotmail.com>
2017-06-14 14:10:51 +0800

    Update header notes

--- MiniFramework/Functions/Global.func.php
+++ MiniFramework/Functions/Global.func.php
@@ -1,11 +1,27 @@
 <?php
-// +------------------------------------------------------------
+// +--------------------------------------------------------------------------------
 // | Mini Framework
-// +------------------------------------------------------------
+// +--------------------------------------------------------------------------------
+// | Copyright (c) 2015-2017 http://www.sunbloger.com
+// +--------------------------------------------------------------------------------
+// | Licensed under the Apache License, Version 2.0 (the "License");
+// | you may not use this file except in compliance with the License.
+// | You may obtain a copy of the License at
+// |
+// |   http://www.apache.org/licenses/LICENSE-2.0
+// |
+// | Unless required by applicable law or agreed to in writing, software
+// | distributed under the License is distributed on an "AS IS" BASIS,
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// | See the License for the specific language governing permissions and
+// | limitations under the License.
+// +--------------------------------------------------------------------------------
 // | Source: https://github.com/jasonweicn/MiniFramework
-// +------------------------------------------------------------
-// | Author: Jason.wei <jasonwei06@hotmail.com>
-// +------------------------------------------------------------
+// +--------------------------------------------------------------------------------
+// | Author: Jason Wei <jasonwei06@hotmail.com>
+// +--------------------------------------------------------------------------------
+// | Website: http://www.sunbloger.com/miniframework
+// +--------------------------------------------------------------------------------
 
 /**
  * 获取客户端IP地址
