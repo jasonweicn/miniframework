fec2d55b7f16e3d5e49e44c86a427eb3f65eaef2
jasonweicn <jasonwei06@hotmail.com>
2018-05-08 17:00:02 +0800

    Update copyright

--- MiniFramework/Functions/Global.func.php
+++ MiniFramework/Functions/Global.func.php
@@ -3,7 +3,7 @@
 // +---------------------------------------------------------------------------
 // | Mini Framework
 // +---------------------------------------------------------------------------
-// | Copyright (c) 2015-2017 http://www.sunbloger.com
+// | Copyright (c) 2015-2018 http://www.sunbloger.com
 // +---------------------------------------------------------------------------
 // | Licensed under the Apache License, Version 2.0 (the "License");
 // | you may not use this file except in compliance with the License.
