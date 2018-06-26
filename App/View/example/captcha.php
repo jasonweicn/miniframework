0cce2e4c97842354fdaf57e5099b59b93f0bbed3
jasonweicn <jasonwei06@hotmail.com>
2018-06-26 17:00:31 +0800

    完善示例代码

--- App/View/example/captcha.php
+++ App/View/example/captcha.php
@@ -1,5 +1,6 @@
 <?php if (isset($this->code)) echo '<p>input code: ' . $this->code . '</p>';?>
 <?php if (isset($this->info)) echo '<p>check result: ' . $this->info . '</p>';?>
+<h1>Example: Captcha</h1>
 <img src="getcaptcha" onclick="this.src='getcaptcha?t='+Math.random()" />
 <form method="post" action="captcha">
   <p>code:<input type="text" name="code" value="" />
--- App/View/example/session.php
+++ App/View/example/session.php
@@ -1,2 +1,3 @@
+<h1>Example: Session</h1>
 <p>Current timestamp: <?php echo $this->t;?></p>
 <p>Session timestamp: <?php echo $this->session_time;?></p>
