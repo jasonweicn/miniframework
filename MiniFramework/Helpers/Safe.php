39b2dc78e693f873d3f59565572f9972b2f7455b
jasonweicn <jasonwei06@hotmail.com>
2019-06-10 14:34:31 +0800

    新增Helpers助手类库

---
+++ MiniFramework/Helpers/Safe.php
@@ -0,0 +1,43 @@
+<?php
+// +---------------------------------------------------------------------------
+// | Mini Framework
+// +---------------------------------------------------------------------------
+// | Copyright (c) 2015-2018 http://www.sunbloger.com
+// +---------------------------------------------------------------------------
+// | Licensed under the Apache License, Version 2.0 (the "License");
+// | you may not use this file except in compliance with the License.
+// | You may obtain a copy of the License at
+// |
+// | http://www.apache.org/licenses/LICENSE-2.0
+// |
+// | Unless required by applicable law or agreed to in writing, software
+// | distributed under the License is distributed on an "AS IS" BASIS,
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// | See the License for the specific language governing permissions and
+// | limitations under the License.
+// +---------------------------------------------------------------------------
+// | Source: https://github.com/jasonweicn/MiniFramework
+// +---------------------------------------------------------------------------
+// | Author: Jason Wei <jasonwei06@hotmail.com>
+// +---------------------------------------------------------------------------
+// | Website: http://www.sunbloger.com/miniframework
+// +---------------------------------------------------------------------------
+namespace Mini\Helpers;
+
+use Mini\Base\Session;
+use Mini\Base\Request;
+
+class Safe
+{
+    public static function getCsrfToken()
+    {
+        $request = Request::getInstance();
+        $token = Session::get($request->getCsrfParamName());
+        
+        if (! $token) {
+            $token = $request->createCsrfToken();
+        }
+        
+        return $token;
+    }
+}
