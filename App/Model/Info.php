c36817316599daf261a659834950301ec71806c0
jasonweicn <jasonwei06@hotmail.com>
2020-02-18 23:53:02 +0800

    删除多余空格

--- App/Model/Info.php
+++ App/Model/Info.php
@@ -20,7 +20,7 @@ class Info extends Model
         //这是手工连接数据库的方法，需要解开顶部 use Mini\Db\Db; 和 use Mini\Base\Config; 两行代码的注释
         //$dbParams = Config::getInstance()->load('database:default');
         //$db = Db::factory('Mysql', $dbParams);
-        
+
         //MiniFramework 从 2.0 开始支持直接调用 Mini\Db\Mysql
         //  需要解开顶部的 user Mini\Db\Mysql; 和 use Mini\Base\Config; 两行代码的注释
         //  这样直接调用的好处是可以让IDE更好地对类的方法进行提示，方便开发者进行编码。
