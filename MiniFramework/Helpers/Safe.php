b326d338379b3ad40dca9b9d7a9723887861f7d4
jasonweicn <jasonwei06@hotmail.com>
2020-12-31 08:44:39 +0800

    更新 copyright 信息

--- MiniFramework/Helpers/Safe.php
+++ MiniFramework/Helpers/Safe.php
@@ -2,7 +2,7 @@
 // +---------------------------------------------------------------------------
 // | Mini Framework
 // +---------------------------------------------------------------------------
-// | Copyright (c) 2015-2020 http://www.sunbloger.com
+// | Copyright (c) 2015-2021 http://www.sunbloger.com
 // +---------------------------------------------------------------------------
 // | Licensed under the Apache License, Version 2.0 (the "License");
 // | you may not use this file except in compliance with the License.
