ec58b0dd3b92d300d449e8ceaca940a9422b4264
jasonweicn <jasonwei06@hotmail.com>
2022-11-19 19:43:15 +0800

    增加路由配置文件

---
+++ App/Config/route-dev.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * 自定义路由配置-开发环境
+ */
+return [
+    'info-<id:\d+>.html' => 'example/route'
+];
---
+++ App/Config/route.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * 自定义路由配置
+ */
+return [
+    'info-<id:\d+>.html' => 'example/route'
+];
