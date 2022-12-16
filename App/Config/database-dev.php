feb16005bfb71c7da4421b20f9d4a51dec9c8939
jasonweicn <jasonwei06@hotmail.com>
2022-12-16 22:51:32 +0800

    更换另一种配置文件编写方式
    最终达到的效果与使用与文件名同名的变量方式一致

--- App/Config/database-dev.php
+++ App/Config/database-dev.php
@@ -1,20 +1,15 @@
 <?php
 /**
  * 数据库配置-开发环境
- * 
- * 你可以在这里定义多组数据库配置信息，例如：
- * $db['db1'] = [...];
- * $db['db2'] = [...];
- * 
- * @var array
  */
-$database['default'] = [
-    'host'          => 'localhost-dev', //主机地址
-    'port'          => 3306,        //端口
-    'dbname'        => 'test',      //库名
-    'username'      => 'root',      //用户名
-    'passwd'        => '',          //密码
-    'charset'       => 'utf8',      //字符编码
-    'persistent'    => false        //是否启用持久连接 （ true | false ）
+return [
+    'default' => [
+        'host'          => 'localhost', //主机地址
+        'port'          => 3306,        //端口
+        'dbname'        => 'test',      //库名
+        'username'      => 'root',      //用户名
+        'passwd'        => '',          //密码
+        'charset'       => 'utf8',      //字符编码
+        'persistent'    => false        //是否启用持久连接
+    ]
 ];
-?>
--- App/Config/database.php
+++ App/Config/database.php
@@ -3,18 +3,17 @@
  * 数据库配置-生产环境
  * 
  * 你可以在这里定义多组数据库配置信息，例如：
- * $db['db1'] = [...];
- * $db['db2'] = [...];
- * 
- * @var array
+ * db1 => [...],
+ * db2 => [...]
  */
-$database['default'] = [
-    'host'          => 'localhost', //主机地址
-    'port'          => 3306,        //端口
-    'dbname'        => 'test',      //库名
-    'username'      => 'root',      //用户名
-    'passwd'        => '',          //密码
-    'charset'       => 'utf8',      //字符编码
-    'persistent'    => false        //是否启用持久连接 （ true | false ）
+return [
+    'default' => [
+        'host'          => 'localhost', //主机地址
+        'port'          => 3306,        //端口
+        'dbname'        => 'test',      //库名
+        'username'      => 'root',      //用户名
+        'passwd'        => '',          //密码
+        'charset'       => 'utf8',      //字符编码
+        'persistent'    => false        //是否启用持久连接
+    ]
 ];
-?>
