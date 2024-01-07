e2d82632d905e5b2ef21a155057c55b7904bf21f
jasonweicn <jasonwei06@hotmail.com>
2024-01-07 22:04:12 +0800

    Update Copyright

--- MiniFramework/Base/Config.php
+++ MiniFramework/Base/Config.php
@@ -2,7 +2,7 @@
 // +---------------------------------------------------------------------------
 // | Mini Framework
 // +---------------------------------------------------------------------------
-// | Copyright (c) 2015-2023 http://www.sunbloger.com
+// | Copyright (c) 2015-2024 http://www.sunbloger.com
 // +---------------------------------------------------------------------------
 // | Licensed under the Apache License, Version 2.0 (the "License");
 // | you may not use this file except in compliance with the License.
